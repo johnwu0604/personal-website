59cb4893cfe113e11a82ffddbac6d13505d4fcbe
John Wu <john.wu@mail.mcgill.ca>
2016-11-24 15:28:32 -0500

    Changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var port  	 = process.env.PORT || 8080;
+var port  	 = process.env.PORT || 5000;
 var database = require('./config/database');
 
 var morgan = require('morgan');
