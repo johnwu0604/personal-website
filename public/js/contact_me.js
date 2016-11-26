8eeb5a5d69b39d65ed521f041ef6ddae50f21d93
John Wu <john.wu@mail.mcgill.ca>
2016-11-25 19:25:33 -0500

    sdfsd

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://john-wu.me/mail/contact_me.php",
+                url: "https://john-wu.herokuapp.com/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
--- public/mail/contact_me.php
+++ public/mail/contact_me.php
@@ -1,4 +1,4 @@
-<?php header('Access-Control-Allow-Origin: http://www.john-wu.me');
+<?php header('Access-Control-Allow-Origin: *');
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
