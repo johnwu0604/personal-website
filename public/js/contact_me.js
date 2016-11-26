651805cca7811f00b12efc626744aeef190c181c
John Wu <john.wu@mail.mcgill.ca>
2016-11-25 19:05:33 -0500

    dsfsd

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://john-wu.me/mail/contact_me.php",
+                url: "john-wu.me/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
